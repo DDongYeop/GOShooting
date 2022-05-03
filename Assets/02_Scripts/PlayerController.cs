50721824f31f361edb4c201ba3a01c02273c25b9
Dongyeop <kitty-777@naver.com>
2022-05-03 11:06:36 +0900

    20220503
    2022년 5월 3일 수업 정리본 입니다

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] 
+    KeyCode keyCodeAttack = KeyCode.Space;
+    [SerializeField] 
+    StageData stageData;
+    
     Movement movement;
+    Weapon weapon;
 
     void Awake()
     {
         movement = GetComponent<Movement>();
+        weapon = GetComponent<Weapon>();
     }
 
 
@@ -18,5 +25,20 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxis("Vertical");
 
         movement.MoveTo(new Vector3(x, y, 0));
+
+        if(Input.GetKeyDown(keyCodeAttack))
+        {
+            weapon.StartFriring();
+        }
+        else if (Input.GetKeyUp(keyCodeAttack))
+        {
+            weapon.StopFiring();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, stageData.LimitMin.x, stageData.LimitMax.x),
+                                         Mathf.Clamp(transform.position.y, stageData.LimitMin.y, stageData.LimitMax.y), 0);
     }
 }
